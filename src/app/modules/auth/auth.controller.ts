0f396ce4e28a66f35fef91c62dff4ae65da5805d
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-05-24 18:54:32 +0600

    up to date

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,8 +4,8 @@ import { ApiResponse } from "../../../utils/ApiResponse";
 import catchAsync from "../../../utils/catchAsync";
 
 const login = catchAsync(async (req: Request, res: Response) => {
-  const result = await AuthService.login(req.body)
-  ApiResponse.success(res, result, "Successfully Login")
+  const result = await AuthService.login(req.body);
+  ApiResponse.success(res, result, "Successfully Login");
 });
 
 const register = catchAsync(async (req: Request, res: Response) => {
@@ -13,20 +13,20 @@ const register = catchAsync(async (req: Request, res: Response) => {
   ApiResponse.success(res, result, "Successfully Register");
 });
 
-const loginUser = catchAsync(async(req: Request, res: Response)=>{
+const loginUser = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.loginUser(req.body);
   ApiResponse.success(res, result, "Successfully Login");
-})
+});
 
-const registerUser = catchAsync(async(req: Request, res: Response)=>{
+const registerUser = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.registerUser(req.body);
   ApiResponse.success(res, result, "Successfully Registered");
-})
-const verifyOtp = catchAsync(async(req: Request, res: Response)=>{
+});
+const verifyOtp = catchAsync(async (req: Request, res: Response) => {
   const { email, otp } = req.body;
-  const result = await AuthService.verifyOtp(email,otp);
+  const result = await AuthService.verifyOtp(email, otp);
   ApiResponse.success(res, result, "Email verified successfully");
-})
+});
 
 export const AuthController = {
   login,
